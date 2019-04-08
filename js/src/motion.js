ff0ba293255d8fcc4c3f114cde4e4699e00a9863
LanTian6 <2473067187@qq.com>
2019-04-08 14:55:34 +0800

    Site updated: 2019-04-08 14:55:27

---
+++ js/src/motion.js
@@ -0,0 +1,29 @@
+var sidebarToggleLine1st = new SidebarToggleLine({
+    el: '.sidebar-toggle-line-first',
+    status: {
+      arrow: {width: '50%', rotateZ: '45deg', top: '2px', left: '6px'},
+      // close: {width: '100%', rotateZ: '-45deg', top: '5px', left: '0px'} //X��
+      close: {width: '50%', rotateZ: '-45deg', top: '2px', left: '0px'} //��ͷ��
+    }
+  });
+  var sidebarToggleLine2nd = new SidebarToggleLine({
+    el: '.sidebar-toggle-line-middle',
+    status: {
+      arrow: {width: '90%'},
+      // close: {opacity: 0} //X��
+      close: {width: '90%'} //��ͷ��
+    }
+  });
+  var sidebarToggleLine3rd = new SidebarToggleLine({
+    el: '.sidebar-toggle-line-last',
+    status: {
+      arrow: {width: '50%', rotateZ: '-45deg', top: '-2px', left: '6px'},
+      // close: {width: '100%', rotateZ: '45deg', top: '-5px', left: '0px'} //X��
+      close: {width: '50%', rotateZ: '45deg', top: '-2px', left: '0px'} //��ͷ��
+    }
+  });
+--------------------- 
+���ߣ�Hunter1023 
+��Դ��CSDN 
+ԭ�ģ�https://blog.csdn.net/weixin_39345384/article/details/80785373 
+��Ȩ����������Ϊ����ԭ�����£�ת���븽�ϲ������ӣ�
